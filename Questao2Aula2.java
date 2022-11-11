7f5d0d7393e73003e331bf6f95a5d5e5ed9f5bd7
John Victor Barros (JV) <jv.jbarros@gmail.com>
2022-11-11 18:32:59 -0300

    Update and rename Exercicio2Aula2 to Questao2Aula2.java

--- Questao2Aula2.java
+++ Questao2Aula2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Exercicio2Aula2 {
+public class Questao2Aula2 {
 
     public static void main(String[] args) {
 //Faça um programa que pegue o nome completo que o usuário digitar e imprima o nome com cada parte separada por espaço, um símbolo de exclamação no início. Exemplo:
